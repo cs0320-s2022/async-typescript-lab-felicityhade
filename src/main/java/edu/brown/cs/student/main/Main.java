73345a523d7610dba208999a2e791a5fca2969cc
felicityhade <78045179+felicityhade@users.noreply.github.com>
2022-03-11 15:25:07 -0500

    commit1

--- src/main/java/edu/brown/cs/student/main/Main.java
+++ src/main/java/edu/brown/cs/student/main/Main.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import com.google.common.collect.ImmutableMap;
@@ -14,6 +16,8 @@ import freemarker.template.Configuration;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import joptsimple.OptionSpec;
+import org.json.JSONException;
+import org.json.JSONObject;
 import spark.*;
 import spark.template.freemarker.FreeMarkerEngine;
 
@@ -80,6 +84,7 @@ public final class Main {
     // Allows requests from any domain (i.e., any URL). This makes development
     // easier, but it’s not a good idea for deployment.
     Spark.before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+    Spark.post("/match", new ResultsHandler());
   }
 
   /**
@@ -111,13 +116,27 @@ public final class Main {
       // and rising
       // for generating matches
 
+      JSONObject jsonObject;
+      String sun = "";
+      String moon = "";
+      String rising = "";
+
+      try{
+        jsonObject = new JSONObject(req.body());
+        sun = jsonObject.getString("sun");
+        moon = jsonObject.getString("moon");
+        rising = jsonObject.getString("rising");
+      } catch (JSONException e) {
+        System.out.println("ERROR: JSON request failed");
+      }
+
       // TODO: use the MatchMaker.makeMatches method to get matches
-
+      List<String> matches = MatchMaker.makeMatches(sun, moon, rising);
       // TODO: create an immutable map using the matches
-
+      Map matchesMap = ImmutableMap.of("matches", matches);
       // TODO: return a json of the suggestions (HINT: use GSON.toJson())
       Gson GSON = new Gson();
-      return null;
+      return GSON.toJson(matchesMap);
     }
   }
 }
--- src/main/site/ts/main.ts
+++ src/main/site/ts/main.ts
@@ -1,21 +1,31 @@
 // TODO: select the list element where the suggestions should go, and all three dropdown elements
 //  HINT: look at the HTML
 
+const suggestionList = document.getElementById("suggestions") as HTMLElement
+const sun = document.getElementById("sun") as HTMLSelectElement
+const moon = document.getElementById("moon") as HTMLSelectElement
+const rising = document.getElementById("rising") as HTMLSelectElement
+
 // Here, when the value of sun is changed, we will call the method postAndUpdate.
 // TODO: Do the same for moon and rising
-
+sun.addEventListener("change", () => {postAndUpdate()})
+moon.addEventListener("change", () => {postAndUpdate()})
+rising.addEventListener("change", () => {postAndUpdate()})
 // TODO: Define a type for the request data object here.
-// type MatchesRequestData = {}
+type MatchesRequestData = {sun : string, moon : string, rising : string}
 
 // TODO: Define a type for the response data object here.
-// type Matches = {}
+type Matches = {[key : string] : string[]}
 
 function postAndUpdate(): void {
   // TODO: empty the suggestionList (you want new suggestions each time someone types something new)
   //  HINT: use .innerHTML
+  // @ts-ignore
+  document.getElementById("suggestions").innerHTML = ""
 
   // TODO: add a type annotation to make this of type MatchesRequestData
-  const postParameters = {
+  const postParameters : MatchesRequestData = {
+    sun : sun.value, moon : moon.value, rising : rising.value
     // TODO: get the text inside the input box
     //  HINT: use sun.value to get the value of the sun field, for example
   };
@@ -26,10 +36,16 @@ function postAndUpdate(): void {
   //  HINT: check out the POST REQUESTS section of the lab and of the front-end guide.
   //  Make sure you add "Access-Control-Allow-Origin":"*" to your headers.
   //  Remember to add a type annotation for the response data using the Matches type you defined above!
+  fetch("http://localhost:8080/results", {
+    method : "POST", body: JSON.stringify(postParameters),
+    headers : {"Content-Type" : "application/json; charset=UTF=8"}
+  })
 
   // TODO: Call and fill in the updateSuggestions method in one of the .then statements in the Promise
   //  Parse the JSON in the response object
   //  HINT: remember to get the specific field in the JSON you want to use
+      .then((response) => response.json())
+      .then((data : Matches) => updateSuggestions(data["data"]))
 }
 
 function updateSuggestions(matches: string[]): void {
@@ -38,6 +54,12 @@ function updateSuggestions(matches: string[]): void {
   //  NOTE: you should use <li> (list item) tags to wrap each element. When you do so,
   //  make sure to add the attribute 'tabindex="0"' (for example: <li tabindex="0">{your element}</li>).
   //  This makes each element selectable via screen reader.
+  console.log(matches)
+  for (let i = 0; i < matches.length; i++) {
+    let match : string = matches[i]
+    console.log(match);
+    suggestionList.innerHTML += `<li tabindex="0">${match}</li>`
+  }
 }
 
 // TODO: create an event listener to the document (document.addEventListener) that detects "keyup".
@@ -45,6 +67,13 @@ function updateSuggestions(matches: string[]): void {
 //  values for the sun, moon, and rising using updateValues. Then call postAndUpdate().
 //  HINT: the listener callback function should be asynchronous and wait until the values are
 //  updated before calling postAndUpdate().
+document.addEventListener("keyup", (event: KeyboardEvent) => {
+  const sunVal = sun.options[sun.selectedIndex].value
+  const moonVal = moon.options[moon.selectedIndex].value
+  const risingVal = rising.options[rising.selectedIndex].value
+  console.log(sunVal, moonVal, risingVal)
+  updateValues(sunVal, moonVal, risingVal)
+})
 
 async function updateValues(sunval: string, moonval: string, risingval: string): Promise<void>{
   // This line asynchronously waits 1 second before updating the values.
